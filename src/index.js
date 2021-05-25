9ae1248b9b1c44a6c5c3e5feb0dd55c415ed6740
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-05-25 09:15:37 -0300

    ReactDOM now renders <App/> component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <h1>Everything is working fine!</h1>,
-  document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
