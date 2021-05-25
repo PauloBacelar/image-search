6400b751cdfdb4775d40479c22cbaf69fe0a2b9e
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-05-25 09:15:13 -0300

    Made basic JSX for input and button

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="ui large icon input">
+          <input type="text" placeholder="Search for images..." />
+          <i className="search icon"></i>
+        </div>
+
+        <button class="ui button">Search</button>
+      </div>
+    );
+  }
+}
+
+export default App;
