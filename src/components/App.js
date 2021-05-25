d525edfa089b335ddf5a32bf942b1d5b6df97082
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-05-25 09:27:05 -0300

    App.js now renders Input component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "../App.css";
+import Input from "./Input";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="ui large icon input">
-          <input type="text" placeholder="Search for images..." />
-          <i className="search icon"></i>
-        </div>
-
-        <button class="ui button">Search</button>
+        <Input />
       </div>
     );
   }
