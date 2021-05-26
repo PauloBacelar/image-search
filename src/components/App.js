8849a45691ec5bd527c9da3f8321127722f6e062
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-05-26 07:25:51 -0300

    Making request to unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,10 @@ import "../App.css";
 import Input from "./Input";
 
 class App extends React.Component {
+  state = {
+    persons: [],
+  };
+
   render() {
     return (
       <div className="container">
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,6 @@
+/* eslint-disable */
 import React from "react";
+import axios from "axios";
 
 class Input extends React.Component {
   constructor() {
@@ -6,10 +8,22 @@ class Input extends React.Component {
     this.state = { inputValue: "" };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this);
   }
 
   onInputChange(event) {
     this.setState({ inputValue: event.target.value });
+  }
+
+  onButtonClick(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: "Client-ID UqWCfpbYqWHntEJ6NPrHdlTLeslIWUmFryJK_cUaB3s",
+      },
+    });
   }
 
   render() {
@@ -25,7 +39,12 @@ class Input extends React.Component {
           <i className="search icon"></i>
         </div>
 
-        <button className="ui button">Search</button>
+        <button
+          className="ui button"
+          onClick={() => this.onButtonClick(this.state.inputValue)}
+        >
+          Search
+        </button>
       </div>
     );
   }
