e68cc37fa81bcbf0efe13cad9a1917e36b47bd96
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-05-27 08:54:19 -0300

    Trying to show image with ImageList and Image components

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,13 +3,10 @@ import React from "react";
 class Image extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      images: [],
-    };
   }
 
   render() {
-    return <h1>Image.JS</h1>;
+    return <img src={this.props.image.urls.regular} />;
   }
 }
 
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,13 +4,14 @@ import Image from "./Image";
 class ImageList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      images: [],
-    };
   }
 
   render() {
-    return <h1>EU SÓ QUERIA TER UM PAL DO TAMAIN DE UM PÉ DE MEZA</h1>;
+    return (
+      <div className="image">
+        <Image image={this.props.images[0].urls} />
+      </div>
+    );
   }
 }
 
