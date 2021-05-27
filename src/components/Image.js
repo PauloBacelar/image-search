b5b30b94cd0ccf8371ed9f20d17bf4ba1312d5aa
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-05-27 08:39:39 -0300

    Created <Image /> component for unique images

---
+++ src/components/Image.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Image extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+    };
+  }
+
+  render() {
+    return <h1>Image.JS</h1>;
+  }
+}
+
+export default Image;
